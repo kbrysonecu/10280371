17594840a56ade0511c24879c2c3fb65d217a97a
Kali <kali@kali>
2021-07-27 04:25:17 -0400

    add Week 2 script

--- week2/passwordCheck.sh
+++ week2/passwordCheck.sh
@@ -7,10 +7,16 @@ echo "Password: $pass_var" | sha256sum > passwordcheck.txt
 #create plain text password into txt 
 
 
-if cmp -s passwordcheck.txt secret.txt; then
+if cmp -s passwordcheck.txt secret.txt; then 
+# compares two .txt documents
+
     echo "Access granted."
     exit 0
+#if they are the same, access is granted
+
 else
     echo "Access denied."
     exit 1
+#if they do not match, then access denied
+
 fi
